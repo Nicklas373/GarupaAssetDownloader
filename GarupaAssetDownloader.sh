d3fb5a536cef14a4a6676c436db37ca82c6caa7a
Dicky Herlambang <herlambangdicky5@gmail.com>
2020-04-03 15:49:17 +0700

    GarupaAssetDownloader: Update patch 1
    * Fixup undefined loop issue
    * Separate loop function to each self function
    * Code cleanup
    
    Signed-off-by: Dicky Herlambang <herlambangdicky5@gmail.com>

--- GarupaAssetDownloader.sh
+++ GarupaAssetDownloader.sh
@@ -19,10 +19,12 @@ Sound="Sound"
 Movie="MV"
 
 function main() {
+echo ""
 echo " Welcome to GarupaAssetDownloader "
 echo " What do you want to do ? "
 echo " 1. Download Assets "
-echo " 2. Extract Assets "
+echo " 2. Join Assets "
+echo ""
 read choice
 
 if [ "$choice" == "1" ];
@@ -30,7 +32,7 @@ if [ "$choice" == "1" ];
 		download
 elif [ "$choice" == "2" ];
 	then
-		extract
+		join
 else
 	main
 fi
@@ -57,6 +59,7 @@ fi
 
 function download() {
 # Begin Downloading
+echo ""
 echo " Which assets that you want to download ?"
 echo " 1. Sound / BGM "
 echo " 2. AMV / MV "
@@ -81,12 +84,14 @@ read answer
 			# Loop for 10 - 99
 			for (( i=10; i<=99; i++))
 				do
+					echo "Downloading Sound Assets..."
 					wget -nc https://res.bandori.ga/assets/sound/bgm0$i
 			done
 
 			# Loop for 100 - 262
 			for (( i=100; i<=262; i++))
 				do
+					echo "Downloading Sound Assets..."
 					wget -nc https://res.bandori.ga/assets/sound/bgm$i
 			done
 
@@ -97,21 +102,41 @@ read answer
 			echo "Please Enter AMV Code"
 			read code
 
-			# Declare HQ AMV Code
-			hq = "_hq"
 			# Downloading Anime MV assets
 			# AMV Assets isn't in order for now, loop argument will not effective in this cases
 			# AMV Assets use uncompressed or RAW assets
 			# So it'll need to decompress manually after complete download
 			cd ..
-			cd Movie
-			echo "Downloading Sound Assets..."
-			wget -nc https://res.bandori.ga/assets/movie/mv/music_video_$code$hq
+			cd MV
+			echo "Downloading AMV Assets..."
+			wget -nc https://res.bandori.ga/assets/movie/mv/music_video_$code"_hq"
 	fi
 
 }
 
-function extract() {
+function looping_phase_1(){	
+# Looping for number 0 - 9	
+for (( j=1; j<=9; j++))	
+	do	
+		 mv $ff-00$j.$fe $ff.00$j	
+	done	
+}	
+function looping_phase_2(){	
+# Looping for number 10 - 99	
+for (( i=10; i<=99; i++ ))	
+	do	
+		mv $ff-0$i.$fe $ff.0$i	
+	done	
+}	
+function looping_phase_3(){	
+# Looping for number 100 - 411	
+for (( k =100; k<=411; k++ ))	
+	do	
+		mv $ff-$k.$fe $ff.$k	
+	done	
+}
+
+function join() {
 # This is looping argument to remove all current file extensions
 # From splitted raw assets files, so it can be readed by hjsplit
 # Then it can merged and encoded by other apps. Like VGMToolbox or AssetStudioGUI
@@ -121,23 +146,14 @@ read ff
 echo "Please Enter file extension"
 read fe
 
-# Looping for number 0 - 9
-for (( j=1; j<=9; j++))
-	do
-		 mv $ff-00$j.$fe $ff.00$j
-	done
-
-# Looping for number 10 - 99
-for (( i=10; i<=99; i++ ))
-	do
-		mv $ff-0$i.$fe $ff.0$i
-	done
-
-# Looping for number 100 - 411
-for (( k =100; k<=411; k++ ))
-	do
-		mv $ff-$k.$fe $ff.$j
-	done
+echo "Looping Phase 1"	
+looping_phase_1	
+echo ""	
+echo "Looping Phase 2"	
+looping_phase_2	
+echo ""	
+echo "Looping Phase 3"	
+looping_phase_3
 }
 
 # Run
